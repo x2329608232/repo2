f86e29452518664066c26c19f2c2c0945a3b7aa9
anyu <2329608232@qq.com>
2020-11-04 18:53:49 +0800

    新建项目

---
+++ git_test_idea/src/com/kfx/Main.java
@@ -0,0 +1,13 @@
+package com.kfx;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("qqqqqqqqqqqqqqqqqqqqqqqq");
+        System.out.println("qqqqqqqqqqqqqqqqqqqqqqqq");
+        System.out.println("qqqqqqqqqqqqqqqqqqqqqqqq");
+        System.out.println("qqqqqqqqqqqqqqqqqqqqqqqq");
+        System.out.println("qqqqqqqqqqqqqqqqqqqqqqqq");
+    }
+}
